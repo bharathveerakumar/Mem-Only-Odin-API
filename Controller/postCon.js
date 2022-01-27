57349561826dc51bd29889a94f1d28ca91a93646
bharathveerakumar <bharathveerakumars@gmail.com>
2022-01-27 13:42:00 +0530

    a

--- Controller/postCon.js
+++ Controller/postCon.js
@@ -8,7 +8,7 @@ const postMess=[
             title:req.body.title,
             body:req.body.body,
             user_id:req.user.user
-        }])
+        }]).then((re)=>console.log(re))
         res.json({"error":"success"})
     }
 ]
